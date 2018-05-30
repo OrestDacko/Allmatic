c0a5b8e680255709b7fd26bb0e6baddd8b21f98d
OrestDacko <orest.datsko@agiliway.com>
2018-05-31 00:20:43 +0300

    styled shop html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,54 @@
 $(document).ready(function() {
-  console.log('ready!');
+  console.log("ready!");
 
-  $('.category-menu-nav-label').click(function() {
+  $(".category-menu-nav-label").click(function() {
     $(this)
-      .toggleClass('active')
+      .toggleClass("active")
       .parent()
-      .children('.category-menu-nav-tree')
+      .children(".category-menu-nav-tree")
       .toggle(200);
   });
 
-  $('.spoiler-trigger').click(function() {
+  $(".spoiler-trigger").click(function() {
     $(this)
       .parent()
       .next()
-      .collapse('toggle');
+      .collapse("toggle");
   });
+
+  $("#filter").bind("click", ".filter-select", function(e) {
+    if (
+      $(e.target).hasClass("material-icons") ||
+      $(e.target).hasClass("chosen-item")
+    ) {
+      $(e.target)
+        .parent()
+        .toggleClass("active")
+        .find(".options")
+        .slideToggle();
+    }
+  });
+  $("#filter").on("submit", function(e) {
+    e.preventDefault();
+    const data = $(this).serialize();
+    const action = $(this).attr("action");
+    const method = $(this).attr("method");
+
+    console.log(data);
+    console.log(action);
+    console.log(method);
+  });
+
+  // $(".select2-multiple").select2MultiCheckboxes({
+  //   width: "auto",
+  //   placeholder: "Your placeholder",
+  //   allowClear: false,
+  //   templateSelection: function(selected, total) {
+  //     return "Selected " + selected.length + " of " + total;
+  //   }
+  // });
 });
+
+$("#q")
+  .val("fsdfsdf")
+  .trigger("change");
