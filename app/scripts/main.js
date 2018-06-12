f997631896fb256ef05b4380aa233115e77a3868
OrestDacko <orest.datsko@agiliway.com>
2018-06-12 20:32:55 +0300

    add media styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,96 +1,96 @@
 $(document).ready(function() {
-  console.log("ready!");
-  $(".category-menu-nav-label").click(function() {
+  console.log('ready!');
+  $('.category-menu-nav-label').click(function() {
     $(this)
-      .toggleClass("active")
+      .toggleClass('active')
       .parent()
-      .children(".category-menu-nav-tree")
+      .children('.category-menu-nav-tree')
       .toggle(200);
   });
 
-  $(".product-details-galery-item").imgZoom({
-    origin: "data-origin",
+  $('.product-details-galery-item').imgZoom({
+    origin: 'data-origin',
     boxWidth: 605,
     boxHeight: 640,
     marginLeft: 30
   });
 
-  var selector = ".top_name";
+  var selector = '.top_name';
 
-  $(".tabgroup > div").hide();
-  $(".tabgroup > div:first-of-type").show();
-  $(".tabs a").click(function(e) {
+  $('.tabgroup > div').hide();
+  $('.tabgroup > div:first-of-type').show();
+  $('.tabs a').click(function(e) {
     e.preventDefault();
     var $this = $(this),
-      tabgroup = "#" + $this.parents(".tabs").data("tabgroup"),
+      tabgroup = '#' + $this.parents('.tabs').data('tabgroup'),
       others = $this
-        .closest("li")
+        .closest('li')
         .siblings()
-        .children("a"),
-      target = $this.attr("href");
-    others.removeClass("active");
-    $this.addClass("active");
+        .children('a'),
+      target = $this.attr('href');
+    others.removeClass('active');
+    $this.addClass('active');
     $(tabgroup)
-      .children("div")
+      .children('div')
       .hide();
     $(target).show();
   });
 
-  $(".top_name").on("click", function() {
-    $(".top_name").removeClass("active");
-    $(this).addClass("active");
+  $('.top_name').on('click', function() {
+    $('.top_name').removeClass('active');
+    $(this).addClass('active');
   });
 
-  $(".spoiler-trigger").click(function() {
+  $('.spoiler-trigger').click(function() {
     $(this)
       .parent()
       .next()
-      .collapse("toggle");
+      .collapse('toggle');
   });
 
-  $("#filter").bind("click", ".filter-select", function(e) {
+  $('#filter').bind('click', '.filter-select', function(e) {
     if (
-      $(e.target).hasClass("material-icons") ||
-      $(e.target).hasClass("chosen-item")
+      $(e.target).hasClass('material-icons') ||
+      $(e.target).hasClass('chosen-item')
     ) {
       $(e.target)
         .parent()
-        .toggleClass("active")
-        .find(".options")
+        .toggleClass('active')
+        .find('.options')
         .slideToggle();
     }
   });
-  $("#filter").on("submit", function(e) {
+  $('#filter').on('submit', function(e) {
     e.preventDefault();
     const data = $(this).serialize();
-    const action = $(this).attr("action");
-    const method = $(this).attr("method");
+    const action = $(this).attr('action');
+    const method = $(this).attr('method');
 
     console.log(data);
     console.log(action);
     console.log(method);
   });
 
-  $(".minus").click(function() {
+  $('.minus').click(function() {
     var $input = $(this)
       .parent()
-      .find("input");
+      .find('input');
     var count = parseInt($input.val()) - 1;
     count = count < 1 ? 1 : count;
     $input.val(count);
     $input.change();
     return false;
   });
-  $(".plus").click(function() {
+  $('.plus').click(function() {
     var $input = $(this)
       .parent()
-      .find("input");
+      .find('input');
     $input.val(parseInt($input.val()) + 1);
     $input.change();
     return false;
   });
 });
 
-$("#q")
-  .val("fsdfsdf")
-  .trigger("change");
+$('#q')
+  .val('fsdfsdf')
+  .trigger('change');
